4d8633dea29554abd83775e2169aec6cdeb5bb49
dbetebenner <dbetebenner@gmail.com>
2014-04-13 13:17:48 -0400

    Adding new RLI baseline matrices

--- Baseline_Coefficient_Matrices/convertToBaseline.R
+++ Baseline_Coefficient_Matrices/convertToBaseline.R
@@ -5,8 +5,8 @@
 ###################################################################################
 
 convertToBaseline <- function(baseline_matrices) {
-	tmp.year <- unlist(strsplit(names(RLI_Baseline_Matrices)[1], "[.]"))[2]
-	names(baseline_matrices) <- gsub(tmp.year, "BASELINE", names(RLI_Baseline_Matrices))
+	tmp.year <- unlist(strsplit(names(baseline_matrices)[1], "[.]"))[2]
+	names(baseline_matrices) <- gsub(tmp.year, "BASELINE", names(baseline_matrices))
 	for (i in names(baseline_matrices)) {
 		for (j in seq_along(baseline_matrices[[i]])) {
 			baseline_matrices[[i]][[j]]@Time <- list(rep("BASELINE", length(unlist(baseline_matrices[[i]][[j]]@Time))))

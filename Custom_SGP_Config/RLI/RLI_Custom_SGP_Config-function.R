d73e356a844963e6f97bd375a12dfbec3cccd792
dbetebenner <dbetebenner@gmail.com>
2014-11-10 16:29:35 -0500

    Updateing SGPstateData

--- Custom_SGP_Config/RLI/RLI_Custom_SGP_Config-function.R
+++ Custom_SGP_Config/RLI/RLI_Custom_SGP_Config-function.R
@@ -74,9 +74,13 @@ function(year,
 				sgp.projection.sequence="EARLY_LIT_FSF"),
 			EARLY_LIT.WS.config=list(
 				sgp.content.areas=c('EARLY_LIT', 'EARLY_LIT', 'EARLY_LIT'),
+				sgp.projection.content.areas=c('EARLY_LIT', 'EARLY_LIT'),
 				sgp.panel.years=c(paste(yearIncrement(year, 0), "1", sep="."), paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
+				sgp.projection.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
 				sgp.grade.sequences=list(c('0.1', '0.2', '0.3'), c('1.1', '1.2', '1.3'), c('2.1', '2.2', '2.3'), c('3.1', '3.2', '3.3')),
-				sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 4))))
+				sgp.projection.grade.sequences=list(c('0.2', '0.3'), c('1.2', '1.3'), c('2.2', '2.3'), c('3.2', '3.3')),
+				sgp.projection.sequence="EARLY_LIT_WSS",
+				sgp.projections.max.forward.progression.years=1))
 		return(tmp.list)
 	} ### END EARLY_LIT/SPRING
 
@@ -85,13 +89,9 @@ function(year,
 		tmp.list <- list(
 			EARLY_LIT.WSS.config=list(
 				sgp.content.areas=c('EARLY_LIT', 'EARLY_LIT', 'EARLY_LIT'),
-				sgp.projection.content.areas=c('EARLY_LIT', 'EARLY_LIT'),
 				sgp.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep="."), paste(yearIncrement(year, 0), "4", sep=".")),
-				sgp.projection.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
 				sgp.grade.sequences=list(c('0.2', '0.3', '0.4'), c('1.2', '1.3', '1.4'), c('2.2', '2.3', '2.4'), c('3.2', '3.3', '3.4')),
-				sgp.projection.grade.sequences=list(c('0.2', '0.3'), c('1.2', '1.3'), c('2.2', '2.3'), c('3.2', '3.3')),
-				sgp.projection.sequence="EARLY_LIT_WSS",
-				sgp.projections.max.forward.progression.years=1))
+				sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 4))))
 		return(tmp.list)
 	} ### END EARLY_LIT/EARLY_SPRING
 
@@ -150,9 +150,13 @@ function(year,
 				sgp.projection.sequence="MATHEMATICS_FSF"),
 			MATHEMATICS.WS.config=list(
 				sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
+				sgp.projection.content.areas=c('MATHEMATICS', 'MATHEMATICS'),
 				sgp.panel.years=c(paste(yearIncrement(year, 0), "1", sep="."), paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
+				sgp.projection.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
 				sgp.grade.sequences=list(c('1.1', '1.2', '1.3'), c('2.1', '2.2', '2.3'), c('3.1', '3.2', '3.3'), c('4.1', '4.2', '4.3'), c('5.1', '5.2', '5.3'), c('6.1', '6.2', '6.3'), c('7.1', '7.2', '7.3'), c('8.1', '8.2', '8.3'), c('9.1', '9.2', '9.3'), c('10.1', '10.2', '10.3'), c('11.1', '11.2', '11.3'), c('12.1', '12.2', '12.3')),
-				sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 12))))
+				sgp.projection.grade.sequences=list(c('1.2', '1.3'), c('2.2', '2.3'), c('3.2', '3.3'), c('4.2', '4.3'), c('5.2', '5.3'), c('6.2', '6.3'), c('7.2', '7.3'), c('8.2', '8.3'), c('9.2', '9.3'), c('10.2', '10.3'), c('11.2', '11.3'), c('12.2', '12.3')),
+				sgp.projection.sequence="MATHEMATICS_WSS",
+				sgp.projections.max.forward.progression.years=1))
 		return(tmp.list)
 	} ### END MATHEMATICS/SPRING
 
@@ -161,13 +165,9 @@ function(year,
 		tmp.list <- list(
 			MATHEMATICS.WSS.config=list(
 				sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
-				sgp.projection.content.areas=c('MATHEMATICS', 'MATHEMATICS'),
 				sgp.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep="."), paste(yearIncrement(year, 0), "4", sep=".")),
-				sgp.projection.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
 				sgp.grade.sequences=list(c('1.2', '1.3', '1.4'), c('2.2', '2.3', '2.4'), c('3.2', '3.3', '3.4'), c('4.2', '4.3', '4.4'), c('5.2', '5.3', '5.4'), c('6.2', '6.3', '6.4'), c('7.2', '7.3', '7.4'), c('8.2', '8.3', '8.4'), c('9.2', '9.3', '9.4'), c('10.2', '10.3', '10.4'), c('11.2', '11.3', '11.4'), c('12.2', '12.3', '12.4')),
-				sgp.projection.grade.sequences=list(c('1.2', '1.3'), c('2.2', '2.3'), c('3.2', '3.3'), c('4.2', '4.3'), c('5.2', '5.3'), c('6.2', '6.3'), c('7.2', '7.3'), c('8.2', '8.3'), c('9.2', '9.3'), c('10.2', '10.3'), c('11.2', '11.3'), c('12.2', '12.3')),
-				sgp.projection.sequence="MATHEMATICS_WSS",
-				sgp.projections.max.forward.progression.years=1))
+				sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 12))))
 		return(tmp.list)
 	} ### END MATHEMATICS/EARLY_SPRING
 
@@ -226,9 +226,13 @@ function(year,
 				sgp.projection.sequence="READING_FSF"),
 			READING.WS.config=list(
 				sgp.content.areas=c('READING', 'READING', 'READING'),
+				sgp.projection.content.areas=c('READING', 'READING'),
 				sgp.panel.years=c(paste(yearIncrement(year, 0), "1", sep="."), paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
+				sgp.projection.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
 				sgp.grade.sequences=list(c('1.1', '1.2', '1.3'), c('2.1', '2.2', '2.3'), c('3.1', '3.2', '3.3'), c('4.1', '4.2', '4.3'), c('5.1', '5.2', '5.3'), c('6.1', '6.2', '6.3'), c('7.1', '7.2', '7.3'), c('8.1', '8.2', '8.3'), c('9.1', '9.2', '9.3'), c('10.1', '10.2', '10.3'), c('11.1', '11.2', '11.3'), c('12.1', '12.2', '12.3')),
-				sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 12))))
+				sgp.projection.grade.sequences=list(c('1.2', '1.3'), c('2.2', '2.3'), c('3.2', '3.3'), c('4.2', '4.3'), c('5.2', '5.3'), c('6.2', '6.3'), c('7.2', '7.3'), c('8.2', '8.3'), c('9.2', '9.3'), c('10.2', '10.3'), c('11.2', '11.3'), c('12.2', '12.3')),
+				sgp.projection.sequence="READING_WSS",
+				sgp.projections.max.forward.progression.years=1))
 		return(tmp.list)
 	} ### END READING/SPRING
 
@@ -237,13 +241,9 @@ function(year,
 		tmp.list <- list(
 			READING.WSS.config=list(
 				sgp.content.areas=c('READING', 'READING', 'READING'),
-				sgp.projection.content.areas=c('READING', 'READING'),
 				sgp.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep="."), paste(yearIncrement(year, 0), "4", sep=".")),
-				sgp.projection.panel.years=c(paste(yearIncrement(year, 0), "2", sep="."), paste(yearIncrement(year, 0), "3", sep=".")),
 				sgp.grade.sequences=list(c('1.2', '1.3', '1.4'), c('2.2', '2.3', '2.4'), c('3.2', '3.3', '3.4'), c('4.2', '4.3', '4.4'), c('5.2', '5.3', '5.4'), c('6.2', '6.3', '6.4'), c('7.2', '7.3', '7.4'), c('8.2', '8.3', '8.4'), c('9.2', '9.3', '9.4'), c('10.2', '10.3', '10.4'), c('11.2', '11.3', '11.4'), c('12.2', '12.3', '12.4')),
-				sgp.projection.grade.sequences=list(c('1.2', '1.3'), c('2.2', '2.3'), c('3.2', '3.3'), c('4.2', '4.3'), c('5.2', '5.3'), c('6.2', '6.3'), c('7.2', '7.3'), c('8.2', '8.3'), c('9.2', '9.3'), c('10.2', '10.3'), c('11.2', '11.3'), c('12.2', '12.3')),
-				sgp.projection.sequence="READING_WSS",
-				sgp.projections.max.forward.progression.years=1))
+				sgp.projection.grade.sequences=as.list(rep("NO_PROJECTIONS", 12))))
 		return(tmp.list)
 	} ### END READING/EARLY_SPRING
 } ### END configSGP

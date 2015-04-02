28baa5d49cc1b108499dcd39ba4c51d17cf3d62f
dbetebenner <dbetebenner@gmail.com>
2015-04-02 12:24:44 -0400

    First changes associated with EARLY_LIT to EARLY_LITERACY naming

--- Cutscores/RLI_Cutscores.R
+++ Cutscores/RLI_Cutscores.R
@@ -9,7 +9,7 @@ SGPstateData[["RLI"]][["Achievement"]][["Cutscores"]] <- list(
 ###  Generic Cutscores.  NOTE:  These are arbitrary dummies set up for testing.
 ###  Per Lindsay Hass (4/22/14 email) only states with linked cutscores are to be used.  No Generic.
 
-	# EARLY_LIT=list(
+	# EARLY_LITERATURE=list(
 		# GRADE_0.1=c(466, 526, 656),
 		# GRADE_0.2=c(466, 526, 656),
 		# GRADE_0.3=c(466, 526, 656),

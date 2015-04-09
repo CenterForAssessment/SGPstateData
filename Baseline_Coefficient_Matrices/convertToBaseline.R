9e512b5095609431f9567e30d4ec33b130ac0816
dbetebenner <dbetebenner@gmail.com>
2015-04-09 08:49:58 -0400

    Update of RLI baseline matrices to include EARLY_LITERACY

--- Baseline_Coefficient_Matrices/convertToBaseline.R
+++ Baseline_Coefficient_Matrices/convertToBaseline.R
@@ -6,13 +6,12 @@
 
 convertToBaseline <- function(baseline_matrices) {
 	tmp.list <- list()
-	tmp.suffix <- paste("[.]", tail(unlist(strsplit(names(baseline_matrices), "[.]")), 1), sep="")
 
 	for (i in names(baseline_matrices)) {
 		for (j in seq_along(baseline_matrices[[i]])) {
 			baseline_matrices[[i]][[j]]@Time <- list(rep("BASELINE", length(unlist(baseline_matrices[[i]][[j]]@Time))))
 		}
-		names(baseline_matrices[[i]]) <- sub(tmp.suffix, "", names(baseline_matrices[[i]]))
+		names(baseline_matrices[[i]]) <- sub("[.][1234]_", "_", names(baseline_matrices[[i]]))
 	}
 
 	tmp.content_areas <- unique(sapply(strsplit(names(baseline_matrices), "[.]"), '[', 1))

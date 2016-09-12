70cd4632f1d914aa82b6e46ab73eba8ea5fe8c1c
dbetebenner <dbetebenner@gmail.com>
2016-09-12 08:40:26 -0400

    Update of Hawaii content area labels for student reports

--- SGPstateData.R
+++ SGPstateData.R
@@ -2495,7 +2495,7 @@ SGPstateData[["HI"]][["Assessment_Program_Information"]][["Assessment_Transition
 			"Met"="Met Standard",
 			"Exceeded"="Exceeded Standard"),
 		Content_Areas_Labels=list(MATHEMATICS="Math", READING="Reading"),
-		Content_Areas_Labels.2015=list(MATHEMATICS="Math", READING="Reading"),
+		Content_Areas_Labels.2015=list(MATHEMATICS="Math", READING="ELA"),
 		Vertical_Scale="No",
 		Vertical_Scale.2015="Yes",
 		Grades_Tested=c(3,4,5,6,7,8,10),
@@ -2510,7 +2510,7 @@ SGPstateData[["HI"]][["Student_Report_Information"]] <-
 #		Transformed_Achievement_Level_Cutscores_gaPlot=list(MATHEMATICS=c(100,200,300,400,500), READING=c(100,200,300,400,500)), ### 2014 and prior
 #		Vertical_Scale="No", ### 2014 and prior
 		Vertical_Scale="Yes",
-		Content_Areas_Labels=list(MATHEMATICS="Math", READING="Reading"),
+		Content_Areas_Labels=list(MATHEMATICS="Math", READING="ELA"),
 #		Grades_Reported=list(MATHEMATICS=c(3,4,5,6,7,8,10), READING=c(3,4,5,6,7,8,10)), ### 2014 and prior
 #		Grades_Reported=list(MATHEMATICS=c(3,4,5,6,7,8,10,11), READING=c(3,4,5,6,7,8,10,11)), ### 2015
 		Grades_Reported=list(MATHEMATICS=c(3,4,5,6,7,8,11), READING=c(3,4,5,6,7,8,11)), ### 2016 and late

6235f694400d7813398b69974e394a7ed99dfc0f
dbetebenner <dbetebenner@gmail.com>
2016-09-19 20:50:09 -0400

    Adding content area projection sequences to NJ_ORIGINAL

--- SGPstateData.R
+++ SGPstateData.R
@@ -5589,6 +5589,31 @@ SGPstateData[["NJ_ORIGINAL"]][["Variable_Name_Lookup"]] <- read.csv("Variable_Na
 SGPstateData[["NJ_ORIGINAL"]][["SGP_Configuration"]] <- list(sgp.loss.hoss.adjustment="NJ_ORIGINAL",
 															print.other.gp=TRUE)
 
+SGPstateData[["NJ_ORIGINAL"]][["SGP_Configuration"]][["grade.projection.sequence"]] <- list(
+	ELA=c("3", "4", "5", "6", "7", "8", "9"),
+	MATHEMATICS=c("3", "4", "5", "6", "7", "8", "EOCT", "EOCT", "EOCT"),
+	GEOMETRY=c("3", "4", "5", "6", "7", "8", "EOCT", "EOCT", "EOCT"),
+	ALGEBRA_I=c("3", "4", "5", "6", "7", "8", "EOCT", "EOCT", "EOCT"),
+	ALGEBRA_II=c("3", "4", "5", "6", "7", "8", "EOCT", "EOCT", "EOCT"))
+SGPstateData[["NJ_ORIGINAL"]][["SGP_Configuration"]][["content_area.projection.sequence"]] <- list(
+	ELA=rep("ELA", 7),
+	MATHEMATICS=c(rep("MATHEMATICS", 6), "ALGEBRA_I", "GEOMETRY", "ALGEBRA_II"),
+	GEOMETRY=c(rep("MATHEMATICS", 6), "ALGEBRA_I", "GEOMETRY", "ALGEBRA_II"),
+	ALGEBRA_I=c(rep("MATHEMATICS", 6), "ALGEBRA_I", "GEOMETRY", "ALGEBRA_II"),
+	ALGEBRA_II=c(rep("MATHEMATICS", 6), "ALGEBRA_I", "GEOMETRY", "ALGEBRA_II"))
+SGPstateData[["NJ_ORIGINAL"]][["SGP_Configuration"]][["year_lags.projection.sequence"]] <- list(
+	ELA=rep(1L, 6),
+	MATHEMATICS=rep(1L, 8),
+	GEOMETRY=rep(1L, 8),
+	ALGEBRA_I=rep(1L, 8),
+	ALGEBRA_II=rep(1L, 8))
+SGPstateData[["NJ_ORIGINAL"]][["SGP_Configuration"]][["max.forward.projection.sequence"]] <- list(
+	ELA=3,
+	MATHEMATICS=3,
+	GEOMETRY=3,
+	ALGEBRA_I=3,
+	ALGEBRA_II=3)
+
 
 ### NEW JERSEY
 

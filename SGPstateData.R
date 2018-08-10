c77fc208bc639e203510c3933bb06087cb2cac55
Damian Betebenner <dbetebenner@nciea.org>
2018-08-10 14:11:37 -0400

    Updating WIDA_WI meta-data

--- SGPstateData.R
+++ SGPstateData.R
@@ -10449,6 +10449,7 @@ SGPstateData[["WIDA_WI"]][["SGP_Configuration"]] <- list(
 	sgp.minimum.default.panel.years=2,
 	max.sgp.target.years.forward=5,
 	sgp.projections.max.forward.progression.years=7,
+	percentile.cuts=c(1,35,50,65,99),
 	print.other.gp=TRUE,
 	calculate.confidence.intervals=list(confidence.quantiles=c(0.025, 0.975)),
 	sgp.target.types=c("Scale_Score_Targets_CUKU", "Scale_Score_Targets_Current_CUKU"))
